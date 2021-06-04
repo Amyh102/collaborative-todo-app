029761c44452a6cd09d9fd9da682f816be939fbc
Parth Modi <parthmodi@Student3.local>
2021-06-04 11:05:31 -0400

    added todo and subscription routes - Parth

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -13,3 +13,39 @@ WHERE username = :username
 -- :doc deletes a user record given the id
 DELETE FROM users
 WHERE username = :username
+
+-- :name create-list! :! :n
+-- :doc creates a new todo list
+INSERT INTO tasks
+(list_id, title, list_of_todos, subscribed_users)
+VALUES (:list_id, :title, :list_of_todos, :subscribed_users)
+
+-- :name update-todos! :! :n
+-- :doc updates todos in a todo list
+UPDATE tasks
+SET list_of_todos = :list_of_todos
+WHERE list_id = :list_id
+
+-- :name add-to-subscription-list! :! :n
+-- :doc updates subscription list of a user
+UPDATE users
+SET subscriptions = ARRAY(SELECT DISTINCT UNNEST(ARRAY_APPEND(subscriptions, :subscription::int)))
+WHERE username = :username
+
+-- :name remove-from-subscription-list! :! :n
+-- :doc updates subscription list of a user
+UPDATE users
+SET subscriptions = ARRAY_REMOVE(subscriptions, :subscription::int)
+WHERE username = :username
+
+-- :name add-to-subscribed-users! :! :n
+-- :doc updates subscribed users in todo list
+UPDATE tasks
+SET subscribed_users = ARRAY(SELECT DISTINCT UNNEST(ARRAY_APPEND(subscribed_users, :subscribed_user::text)))
+WHERE list_id = :list_id
+
+-- :name remove-from-subscribed-users! :! :n
+-- :doc updates subscribed users in todo list
+UPDATE tasks
+SET subscribed_users = ARRAY_REMOVE(subscribed_users, :subscribed_user::text)
+WHERE list_id = :list_id
